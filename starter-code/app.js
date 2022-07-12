46e207f400688ca64d290a45d1d75d7fa6c39809
Rambey <71680392+rambey-coder@users.noreply.github.com>
2022-07-12 09:06:07 +0100

    code cleanup

--- starter-code/app.js
+++ starter-code/app.js
@@ -47,32 +47,11 @@ const setMoon = (value) => {
 `
 
     const img = document.createElement('img')
+    img.classList.add('active')
     img.src = val.images.png;
     imgContainer.appendChild(img)
 }
 
-// const setMars = (value) => {
-//     const val = value.destinations[1];
-//     tabContent.innerHTML += ` 
-    // <h1>${val.name}</h1>
-    // <p>${val.description}</p>
-    // <hr>
-    // <div class='distance'>
-    //     <div class='dis-1'>
-    //         <P>AVG. DISTANCE</P>
-    //         <h2>${val.distance}</h2>
-    //     </div>
-    //     <div class='dis-2'>
-    //         <P>AVG. DISTANCE</P>
-    //         <h2>${val.travel}</h2>
-    //     </div>
-    // </div>
-// `
-
-    // const img = document.createElement('img')
-    // img.src = val.images.png;
-    // imgContainer.appendChild(img)
-// }
 
 const setMars = (value) => {
     const container = document.createElement('div')
